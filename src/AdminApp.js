9da6cab1db9972ad39da26904c02602bf391fbad
Josh Pollock <josh@calderawp.com>
2018-08-18 15:10:45 -0400

    missing docs

--- src/AdminApp.js
+++ src/AdminApp.js
@@ -5,10 +5,18 @@ import {CalderaAdminWithState} from "./CalderaAdminWithState";
 import {Provider} from 'react-redux';
 export default function AdminApp(){
 	let domElement = null;
+
+	/**
+	 * Get the current Redux(-like) store
+	 */
 	this.getStore = function () {
 		return store;
 	};
 
+	/**
+	 * Render admin app to dom
+	 * @param {String} element ID attribute of element to mount app on
+	 */
 	this.renderToDom = function( element ){
 
 		ReactDOM.render(
